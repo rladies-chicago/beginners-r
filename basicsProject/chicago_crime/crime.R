c7c81d28c8d3cf21979a1100ca45e00a4af2dd04
Zaynaib Giwa <zaynaibg@gmail.com>
2019-06-28 09:45:05 -0500

    chicago crime data

---
+++ basicsProject/chicago_crime/crime.R
@@ -0,0 +1,16 @@
+
+
+#Load the csv data
+
+#How many rows of data (observations) are in this dataset?
+
+#How many variables are in this dataset?
+
+#Using the "max" function, what is the maximum value of the variable "ID"?
+
+#What is the minimum value of the variable "Beat"?
+
+#How many observations have value TRUE in the Arrest variable (this is the number of crimes for which an arrest was made)?
+
+#How many observations have a LocationDescription value of ALLEY?
+
